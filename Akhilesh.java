03c569a84935ebf06bd287197607a54ebf62b74f
Akhilesh <akpatil1472001@gmail.com>
2023-09-29 18:15:38 +0530

    first commit

---
+++ Akhilesh.java
@@ -0,0 +1,33 @@
+class Akhilesh{
+	static float height;
+	static long ph;
+	static byte age;
+	static double weight;
+	static String address;
+	static int salary;
+	static int fees;
+	static String name;
+	static String color;
+	static int id;
+	static String lname;
+	static String fname;
+	static char section;
+	static String className;
+	static int usn;
+	static String location;
+	static String schoolAdd;
+	static String school;
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Details of Akhilesh");
+		System.out.println(Akhilesh.height);
+		System.out.println(Akhilesh.ph);
+		System.out.println(Akhilesh.age);
+		System.out.println(Akhilesh.weight);
+		System.out.println(Akhilesh.address);
+		System.out.println(Akhilesh.salary);
+		
+	}
+}
